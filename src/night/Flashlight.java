59fac05b2254876ca56c3e7b0ac6a9ffc77737bb
Fire <annabeth.phoenix@gmail.com>
2023-02-17 04:49:52 -0600

    added flashlight on/off toggle

--- src/night/Flashlight.java
+++ src/night/Flashlight.java
@@ -9,6 +9,11 @@ public class Flashlight extends GameObject {
 	final private static int minradius = 5, maxradius = 30;
 	private int radius;
 	
+	final private static float batterySpeed = 0.025f;
+	private float battery = 1; // we're going to do a thing and have this be
+							   // between 0 and 1
+	private boolean on = true;
+	
 	public Flashlight(Player owner_, int x_, int y_) {
 		super(x_, y_, minradius*2, minradius*2);
 		
@@ -18,18 +23,51 @@ public class Flashlight extends GameObject {
 	
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(new Color(255, 255, 150, 100)); // yellow but translucent
-		g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+		if (on) {
+			g.setColor(new Color(255, 255, 150, 100)); // yellow but translucent
+			g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+		}
+		else {
+			g.setColor(Color.lightGray);
+			g.drawOval(x - radius, y - radius, radius * 2, radius * 2);
+		}
+		
+		// The battery
+		g.setColor(Color.white);
+		g.drawRect(5, 5, 200, 20);
+		g.fillRect(5, 5, (int) (battery * 200), 20);
 	}
 	
-	public void setPos(int x_, int y_) {
+	private void setPos(int x_, int y_) {
 		x = x_;
 		y = y_;
 	}
 	
-	public void updateRadius(int amt) {
+	private void updateRadius(int amt) {
 		radius += amt;
 		if (radius < minradius) radius = minradius;
 		else if (radius > maxradius) radius = maxradius;
+	}
+	
+	public void update(int x_, int y_, boolean mouseHeld) {
+		setPos(x_, y_);
+		
+		if (battery > 0) on = mouseHeld;
+		
+		if (on) {
+			updateRadius(1);
+			battery -= batterySpeed;
+			if (battery <= 0) {
+				// radius = minradius;
+				on = false;
+				battery = 0;
+			}
+		} else {
+			battery += batterySpeed;
+			updateRadius(-2);
+			if (battery > 1) {
+				battery = 1;
+			}
+		}
 	}
 }
